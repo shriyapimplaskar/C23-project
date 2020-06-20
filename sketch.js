3b38c04b46dbad9e74ec1cf524aa0149bc6d5316
shriyapimplaskar <64077246+shriyapimplaskar@users.noreply.github.com>
2020-06-20 16:53:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400,400);
+
+}
+
+function draw() {
+
+  background(255,255,255);  
+rectMode(CENTER);
+ellipseMode(RADIUS);
+rect (200,300,100,200);
+ellipse(200,32030,30);
+rect(65,343,100,150);
+rect(335,357,100,175);
+rect(265,340,50,175);
+rect(127,340,50,175);
+triangle(149,178,247,178,197,74);
+rect(200,200,100,40);
+
+
+
+
+
+  drawSprites(); 
+}
